e0c2d9bb32621128dd68dab69d2f92ba9745a2d3
JnKindle <Jn_never2013@163.com>
2018-08-23 11:17:05 +0800

    Initial Commit

---
+++ MGHRouter+WeiXinShake/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MGHRouter+WeiXinShake
+//
+//  Created by Jn_Kindle on 2018/8/23.
+//  Copyright © 2018年 JnKindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
